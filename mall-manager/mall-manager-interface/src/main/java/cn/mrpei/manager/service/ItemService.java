d3a5fb93025c6351d03e7a6e5ca70792b126ab49
peizhouyu <peizhouyu@live.com>
2018-05-16 19:43:24 +0800

    add service

---
+++ mall-manager/mall-manager-interface/src/main/java/cn/mrpei/manager/service/ItemService.java
@@ -0,0 +1,14 @@
+package cn.mrpei.manager.service;
+
+/**
+ * @author peizhouyu (大数据部-大数据研发部-平台产品研发部)
+ * @version V1.0.0
+ * @description
+ * @date 2018/5/16
+ * @last-modified ：
+ * @class cn.mrpei.manager.service
+ * @copyright Copyright © 2004-2018 京东JD.com ALL Right Reserved
+ * @see
+ */
+public interface ItemService {
+}
